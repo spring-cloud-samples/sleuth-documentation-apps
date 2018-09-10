78a6c4b9d3d81a08be0c87d14ddfed9ee31549a8
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2018-09-10 14:44:39 +0200

    Please work

--- scripts/kill.sh
+++ scripts/kill.sh
@@ -2,4 +2,4 @@
 
 kill `jps | grep "1.0.0.SLEUTH_DOCS.jar" | cut -d " " -f 1` || echo "No apps running"
 kill `jps | grep "zipkin.jar" | cut -d " " -f 1` || echo "No zipkin running"
-docker ps -a -q | xargs -n 1 -P 8 -I {} docker stop {} || echo "No docker containers running"
+docker ps -a -q | xargs -n 1 -P 8 -I {} docker kill {} || echo "No docker containers running"
