1c249148d8089f61e89fed096cc22915a40c7511
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2018-09-10 14:35:29 +0200

    Fixed wrong merge

--- scripts/kill.sh
+++ scripts/kill.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-<<<<<<< HEAD
-kill `jps | grep "1.0.0.SLEUTH_DOCS.jar" | cut -d " " -f 1`
-kill `jps | grep "zipkin.jar" | cut -d " " -f 1`
-docker-compose kill || echo "Failed to kill docker compose started apps"
-=======
 kill `jps | grep "1.0.0.SLEUTH_DOCS.jar" | cut -d " " -f 1` || echo "No apps running"
 kill `jps | grep "zipkin.jar" | cut -d " " -f 1` || echo "No zipkin running"
 docker ps -a -q | xargs -n 1 -P 8 -I {} docker stop {} || echo "No docker containers running"
->>>>>>> c80fe9e... Please work
