b06a6f0f071a2f54e85c0f6ccd4407144bafacd8
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2018-10-27 15:14:03 +0200

    Fixed wrong deps

--- service1/src/main/java/io/spring/cloud/sleuth/docs/service1/Application.java
+++ service1/src/main/java/io/spring/cloud/sleuth/docs/service1/Application.java
@@ -1,15 +1,9 @@
 package io.spring.cloud.sleuth.docs.service1;
 
-import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
-import zipkin2.reporter.Sender;
 
 @SpringBootApplication
 public class Application {
