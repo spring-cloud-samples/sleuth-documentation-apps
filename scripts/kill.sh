7b4c101af8256f1cf32dc619cbf7804d2c685cbb
Marcin Grzejszczak <marcin@grzejszczak.pl>
2018-12-20 10:44:54 +0100

    Added debug

--- scripts/kill.sh
+++ scripts/kill.sh
@@ -9,9 +9,15 @@ function kill_app_at_port() {
   kill -9 $(lsof -t -i:$1) && echo "Killed an app running on port [$1]" || echo "No app running on port [$1]"
 }
 
+echo "Running apps:"
+jps | grep "1.0.0.SLEUTH_DOCS.jar"
+
+echo "Running docker processes"
+docker ps
+
 kill `jps | grep "1.0.0.SLEUTH_DOCS.jar" | cut -d " " -f 1` || echo "No apps running"
 kill `jps | grep "zipkin.jar" | cut -d " " -f 1` || echo "No zipkin running"
-kill_app_at_port ${SERVICE1_PORT} || echo "Failed to kill service"
-kill_app_at_port ${SERVICE2_PORT} || echo "Failed to kill service"
-kill_app_at_port ${SERVICE3_PORT} || echo "Failed to kill service"
-kill_app_at_port ${SERVICE4_PORT} || echo "Failed to kill service"
+kill_app_at_port ${SERVICE1_PORT} && echo "Killed app at port ${SERVICE1_PORT}" || echo "Failed to kill service"
+kill_app_at_port ${SERVICE2_PORT} && echo "Killed app at port ${SERVICE2_PORT}" || echo "Failed to kill service"
+kill_app_at_port ${SERVICE3_PORT} && echo "Killed app at port ${SERVICE3_PORT}" || echo "Failed to kill service"
+kill_app_at_port ${SERVICE4_PORT} && echo "Killed app at port ${SERVICE4_PORT}" || echo "Failed to kill service"
