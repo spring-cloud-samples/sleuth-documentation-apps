2434095b831dc6ae56603d66f4cd8fc50ff3444a
Marcin Grzejszczak <marcin.grzejszczak@gmail.com>
2021-09-01 13:21:27 +0200

    Working example with logs and tracing and metrics

--- presentation-service-observability/src/main/java/com/example/presentationservice/PresentationServiceApplication.java
+++ presentation-service-observability/src/main/java/com/example/presentationservice/PresentationServiceApplication.java
@@ -5,15 +5,18 @@ import java.util.Random;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.observability.event.Recorder;
 import org.springframework.core.observability.event.instant.InstantEvent;
+import org.springframework.core.observability.event.instant.InstantRecording;
 import org.springframework.core.observability.event.interval.IntervalEvent;
 import org.springframework.core.observability.event.interval.IntervalRecording;
 import org.springframework.core.observability.event.tag.Cardinality;
 import org.springframework.core.observability.event.tag.Tag;
+import org.springframework.core.observability.tracing.Tracer;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -38,6 +41,8 @@ class PresentationController {
 	private static final Logger log = LoggerFactory.getLogger(PresentationController.class);
 
 	private final RestTemplateService service;
+
+	@Autowired Tracer tracer;
 
 	private final Random random = new Random();
 
@@ -70,7 +75,9 @@ class RestTemplateService {
 				.highCardinalityName("calling-start " + param)
 				.start();
 		try {
+			// ...
 			this.recorder.recordingFor((InstantEvent) () -> "calling-start.before-call").record();
+			// ...
 			return this.restTemplate.postForObject("http://localhost:9081/start", "", String.class);
 		} catch (Exception ex) {
 			intervalRecording.error(ex);
